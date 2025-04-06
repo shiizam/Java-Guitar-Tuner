e39250543b1f0e0ff3dbd0ac9251ad6a69440f51
Cody <codyhancock212@yahoo.com>
2025-04-06 15:52:20 -0400

    Tracking current bug: add logic to skip low energy signals

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -143,6 +143,8 @@ public class Main {
 
         double difference = detectedFreq - targetFreq;
 
+        System.out.println("Difference: " + difference);
+
 
         // Determine tuning status
 
